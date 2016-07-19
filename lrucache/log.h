0442db9b4ad9772916bea916380215b1d4428190
miaohongyuan <miaohongyuan@autohome.com.cn>
2016-07-19 13:45:21 +0800

    lrucache: add log.h which encapsulate log4cpp

---
+++ lrucache/log.h
@@ -0,0 +1,15 @@
+/*
+ * log.h
+ *
+ *  Created on: 2016年7月19日
+ *      Author: linus
+ */
+
+#ifndef CACHE_LRUCACHE_LOG_H_
+#define CACHE_LRUCACHE_LOG_H_
+
+
+
+
+
+#endif /* CACHE_LRUCACHE_LOG_H_ */
